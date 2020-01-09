97727c30b79c26131339e554a2f328bec76fc446
amikhailov <amikhailov@101media.ru>
2020-01-09 14:05:16 +0300

    fix

--- parser.php
+++ parser.php
@@ -1,18 +1,23 @@
 <?php
 
 use Clue\React\Buzz\Browser;
+use Clue\React\Socks\Client as SocksClient;
 use Symfony\Component\DomCrawler\Crawler;
 
 require 'vendor/autoload.php';
 
 $loop = React\EventLoop\Factory::create();
+
+//$proxy = new SocksClient('184.178.172.13:15311', new \React\Socket\Connector($loop));
+//$client = new Browser($loop, new \React\Socket\Connector($loop, ['tcp' => $proxy]));
+
 $client = new Browser($loop);
 
-$client->get('https://understat.com/team/Liverpool')
-    ->then(function(\Psr\Http\Message\ResponseInterface $response) {
-        $crawler = new Crawler((string) $response->getBody());
+$scripts = [];
 
-        $scripts = [];
+$client->get('https://understat.com/league/EPL')
+    ->then(function(\Psr\Http\Message\ResponseInterface $response) use (&$scripts) {
+        $crawler = new Crawler((string) $response->getBody());
 
         $crawler->filter('script')->each(function (Crawler $node, $i) use (&$scripts) {
 
@@ -24,21 +29,18 @@ $client->get('https://understat.com/team/Liverpool')
 
                 $json = $matches[1][0];
 
-                $data = preg_replace_callback('#\\\\x([[:xdigit:]]{2})#ism', function($matches)
-                {
+                $data = preg_replace_callback('#\\\\x([[:xdigit:]]{2})#ism', function($matches) {
                     return chr(hexdec($matches[1]));
-                },
-                    $json);
+                }, $json);
 
                 $scripts[] = json_decode($data, true);
             }
         });
 
-
-
-        var_dump(count($scripts), count($scripts[0]),  count($scripts[1]),  count($scripts[2]));die();
-
-
+    })->otherwise(function (Exception $exception) {
+        var_dump($exception->getMessage());
     });
 
 $loop->run();
+
+var_dump(count($scripts), count($scripts[0]),  count($scripts[1]),  count($scripts[2]));die();
